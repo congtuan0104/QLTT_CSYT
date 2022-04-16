fa037c80fe660e92a25e84447112e7ffaf7e5c5e
LÊ HÙNG SƠN <19120349@student.hcmus.edu.vn>
2022-04-16 20:20:17 +0700

    ADD USER/ROLE

--- Admin_AddUser.xaml.cs
+++ Admin_AddUser.xaml.cs
@@ -11,7 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using Oracle.DataAccess.Client;
+using System.Data;
 namespace QLTT_CSYT
 {
     /// <summary>
@@ -22,6 +23,76 @@ namespace QLTT_CSYT
         public Admin_AddUser()
         {
             InitializeComponent();
+           
+            
         }
+        
+
+        
+
+        private void btnName_Click(object sender, RoutedEventArgs e)
+        {
+            if(user.IsChecked==true )
+            {
+                string username = txbUser.Text.ToString();
+                string password = txbPass.Text.ToString();
+                string role = cbRole.Text.ToString();
+                string sql;
+                
+                sql = "ALTER SESSION SET \"_ORACLE_SCRIPT\" = true";
+                Class.Functions.RunSQL(sql);
+
+                if (((username[0] >= 'a') && (username[0] <= 'z')) || ((username[0] >= 'A') && (username[0] <= 'Z')))
+                {
+                    sql = "CREATE USER " + username + " IDENTIFIED BY " + password;
+                }
+                else
+                {
+                    sql = "CREATE USER " + "\"" + username + "\"" + " IDENTIFIED BY " + password;
+                }
+
+
+     
+                Class.Functions.RunSQL(sql);
+
+                if (role != "KHONG")
+                {
+                    if (((username[0] >= 'a') && (username[0] <= 'z')) || ((username[0] >= 'A') && (username[0] <= 'Z')))
+                    {
+                        sql = "GRANT " + role + " TO " + username;
+                    }
+                    else
+                    {
+                        sql = "GRANT " + role + " TO " + "\"" + username + "\"";
+                    }
+ 
+                    Class.Functions.RunSQL(sql);
+                }
+              
+
+                this.Close();
+                
+
+            }
+
+
+
+            if(role.IsChecked==true )
+            {
+                string rolename = txbrole.Text.ToString();
+                string sql;
+                sql = "ALTER SESSION SET \"_ORACLE_SCRIPT\" = true";
+                Class.Functions.RunSQL(sql);
+                sql = "CREATE ROLE " + rolename;
+                MessageBox.Show(sql);
+                Class.Functions.RunSQL(sql);
+              
+                
+            }
+            
+
+        }
+
+
     }
 }
